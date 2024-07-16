c61790eafd24a8d77de40654440798fcaae4cca2
Rafael Silveira <144189737+Ardonitos@users.noreply.github.com>
2024-07-16 19:37:13 -0300

    Modified Program Entrypoint

--- BlackJack/Game.cs
+++ BlackJack/Game.cs
@@ -1,12 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace BlackJack
 {
     internal class Game
     {
+        private static Actors Dealer = new();
+        private static Actors Player = new();
+
+        public static void Run()
+        {
+            Menu(dealer: true);
+        }
+        private static void Menu(bool dealer)
+        {
+            Lin("=");
+            Console.WriteLine("        BlackJack Game");
+            Lin("=");
+            // Displaying the cards
+            if (dealer)
+            {
+                Console.WriteLine($"Dealer: {CardDisplay(Dealer.Deck, dealer)}");
+            }
+            else
+            {
+                Console.WriteLine($"Dealer: {CardDisplay(Dealer.Deck, false)}" +
+                    $"| {Dealer.Total()}");
+            }
+            Console.WriteLine($"Player: {CardDisplay(Player.Deck, false)}" +
+                $"| {Player.Total()}");
+            Console.WriteLine("[ 1 ] HIT  |  [ 2 ] STAND");
+            Lin("=");
+        }
+        private static string CardDisplay(List<Card> deck, bool dealer)
+        {
+            int count = 0;
+            string stringDeck = "";
+
+            foreach (Card card in deck)
+            {
+                if (dealer && count < 1)
+                {
+                    card.Hide();
+                    count++;
+                }
+
+                stringDeck += card.ToString();
+                stringDeck += " ";
+            }
+            return stringDeck;
+        }
+        private static void Lin(string symbol)
+        {
+            string lin = symbol;
+            for (int i = 1; i <= 29; i++)
+            {
+                lin += symbol;
+            }
+            Console.WriteLine(lin);
+        }
     }
 }
--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
+﻿
 namespace BlackJack
 {
     class Program
@@ -8,64 +7,6 @@ namespace BlackJack
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Game.Run();
-        }
-
-    }
-    class Game { 
-
-        private static Actors Dealer = new();
-        private static Actors Player = new();
-
-        public static void Run()
-        {
-            Menu(dealer: true);
-        }
-        private static void Menu(bool dealer)
-        {
-            Lin("=");
-            Console.WriteLine("        BlackJack Game");
-            Lin("=");
-            // Displaying the cards
-            if (dealer)
-            {
-                Console.WriteLine($"Dealer: {CardDisplay(Dealer.Deck, dealer)}");
-            }
-            else
-            {
-                Console.WriteLine($"Dealer: {CardDisplay(Dealer.Deck, false)}" +
-                    $"| {Dealer.Total()}");
-            }
-            Console.WriteLine($"Player: {CardDisplay(Player.Deck, false)}" +
-                $"| {Player.Total()}");
-            Console.WriteLine("[ 1 ] HIT  |  [ 2 ] STAND");
-            Lin("=");
-        }
-        private static string CardDisplay(List<Card> deck, bool dealer)
-        {
-            int count = 0;
-            string stringDeck = "";
-
-            foreach (Card card in deck)
-            {
-                if (dealer && count < 1)
-                {
-                    card.Hide();
-                    count++;
-                }
-
-                stringDeck += card.ToString();
-                stringDeck += " ";
-            }
-            return stringDeck;
-        }
-        private static void Lin(string symbol)
-        {
-            string lin = symbol;
-            for (int i = 1; i <= 29; i++)
-            {
-                lin += symbol;
-            }
-            Console.WriteLine(lin);
         }
     }
 }
